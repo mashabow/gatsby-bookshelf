dd83a89f528fdef97e8f1058a3b2cb4b08ade7dc
Masaya Nakamura <mashabow@shiromoji.net>
2018-10-18 00:59:49 +0900

    Implement a simple table view

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,48 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
+export default ({ data }) => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>本の一覧</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>タイトル</th>
+          <th></th>
+          <th>購入日</th>
+          <th>読了日</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.allSampleCsv.edges.map(({ node }, index) => (
+          <tr key={index}>
+            <td>
+              {node.title}
+            </td>
+            <td>{node.linkUrl ? <a href={node.linkUrl}>Amazon</a> : '-'}</td>
+            <td>{node.purchaseDate}</td>
+            <td>{node.finishDate}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
-export default IndexPage
+export const query = graphql`
+  query {
+    allSampleCsv {
+      edges {
+        node {
+          title
+          linkUrl
+          purchaseDate
+          finishDate
+        }
+      }
+    }
+  }
+`;
