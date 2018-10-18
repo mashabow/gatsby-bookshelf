269c939ff867fd90b632fa02912ff59189dc12f4
Masaya Nakamura <mashabow@shiromoji.net>
2018-10-18 12:31:12 +0900

    Remove page-2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
@@ -28,7 +28,6 @@ export default ({ data }) => (
         ))}
       </tbody>
     </table>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
